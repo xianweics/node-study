400f0a5ce4dbbf4169a877f2a0599edd48e138ac
xianweics <503158245@qq.com>
2017-08-08 22:13:54 +0800

    no message

---
+++ demo5_1.js
@@ -0,0 +1,9 @@
+function user() {
+  return 'user'
+}
+module.exports=user;
+// exports.user=function () {
+//   return 'user';
+// };
+
+
---
+++ demo5_2.js
@@ -0,0 +1,3 @@
+let user=require("./demo5_1");
+console.info(user());
+// console.info(user.user());
